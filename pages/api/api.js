9c3f7b3a7968009d0a8aafda31abcf5f34f27512
Enrique Torreblanca <enriquetchat@gmail.com>
2024-09-09 22:08:22 -0700

    Add pages/api/api.js

---
+++ pages/api/api.js
@@ -0,0 +1,28 @@
+```javascript
+// pages/api/locations.js
+
+export default function handler(req, res) {
+  // Create sample data for two locations with latitude and longitude
+  const locations = [
+    {
+      id: 1,
+      name: 'Location 1',
+      coordinates: {
+        latitude: 40.712776,
+        longitude: -74.005974
+      }
+    },
+    {
+      id: 2,
+      name: 'Location 2',
+      coordinates: {
+        latitude: 34.052235,
+        longitude: -118.243683
+      }
+    }
+  ];
+
+  // Send the locations as JSON
+  res.status(200).json(locations);
+}
+```
